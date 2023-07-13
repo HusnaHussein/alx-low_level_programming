26d03c0caaa787345776e6844d482273dd3e8adb
HusnaHussein <husnahussainkilalo@gmail.com>
2023-07-13 01:06:48 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0.
+*/
+int main(void)
+{
+        printf("Programming is like building a multilingual puzzle\n");
+        return (0);
+}
