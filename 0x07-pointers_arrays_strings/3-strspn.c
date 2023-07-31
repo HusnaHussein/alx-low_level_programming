1a3e14dd05e578423f77dc1f4bc50f471ca1ea45
HusnaHussein <husnahussainkilalo@gmail.com>
2023-07-31 03:34:44 -0700

    My primary goal of hacking was the intellectual curiosity, the seduction of adventure

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,30 +2,23 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: target segment
- * @accept: reference
- *
- * Retrun: number of bytes in the initial segment of s
- * which consist only of bytes from accept
+ * @s: input string to search for substring
+ * @accept: characters that prefix substring must include
+ * Return: length of prefix substring
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count1, count2;
+	unsigned int i, j, a_len = 0, len = 0;
 
-	for (count1 = 0; s[count1] != '\0'; count1++)
-	{
-		for (count2 = 0; accept[count2] != '\0'; count2++)
-		{
-			if (s[count1] == accept[count2])
-			{
-				break;
-			}
-		}
-			if (!(accept[count2]))
-			{
-				break;
-			}
-	}
-	return (count1);
+	while (accept[a_len] != '\0')
+		a_len++;
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < a_len; j++)
+			if (s[i] == accept[j])
+				len++, j = a_len;
+			else
+				if (j == a_len - 1)
+					goto exit;
+exit: return (len);
 }
