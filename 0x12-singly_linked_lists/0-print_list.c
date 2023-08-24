5dd6769ffa75b4901d4b1d0cdc186628bf4c8286
HusnaHussein <husnahussainkilalo@gmail.com>
2023-08-24 06:41:00 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,22 +11,22 @@
 */
 size_t print_list(const list_t *h)
 {
-   int count = 0;
+	int count = 0;
 
 
-   while (h)
-   {
-       if (h->str == NULL)
-       {
-           printf("[0] (nil)\n");
-       }
-       else
-       {
-       printf("[%d] %s\n", h->len, h->str);
-       }
-       count++;
-       h = h->next;
-   }
-   return (count);
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
 
