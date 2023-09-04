b034db024ba216b4683a8fb17e64404ea59b1819
HusnaHussein <husnahussainkilalo@gmail.com>
2023-09-04 11:18:43 -0700

    File handling

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: name of the file
+* @letters: number of letters to read and print
+*
+* Return: number of letter read and printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	
+}
