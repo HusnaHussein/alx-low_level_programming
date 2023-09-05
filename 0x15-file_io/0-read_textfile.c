846d780467a5919f5c0f80aa27f0a794b32cc0ea
HusnaHussein <husnahussainkilalo@gmail.com>
2023-09-04 23:40:48 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,4 +9,33 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buffer;
+	int fd_open;
+	ssize_t fd_read, fd_write;
+
+	if (filename == NULL)
+		return (0);
+	fd_open = open(filename, O_RDONLY);
+	if (fd_open == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	fd_read = read(fd_open, buffer, letters);
+	if (fd_read == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	fd_write = write(STDOUT_FILENO, buffer, fd_read);
+	if (fd_write == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	close(fd_open);
+	free(buffer);
+	return (fd_write);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
